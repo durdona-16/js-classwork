7b049df5a29fe05a3cb007a81f75ee1e4de2a6f9
shohpolat <shohruxbeka@mail.ru>
2023-01-29 18:35:29 +0500

    new log added

---
+++ 2-dars/index.js
@@ -0,0 +1,99 @@
+//  let name="ilya";
+// console.log(name)
+
+// console.log(`hello ${1}`)
+// console.log(`hello ${name}`)
+// console.log(`hello ${"name"}`)
+// let undefined=2
+// console.log()
+
+
+
+// let a= 1
+// let b= "1.1"
+
+
+// console.log(typeof typeof a);
+// console.log(a+b);
+// console.log(a+ +b);
+// console.log(a+ +parseInt(b));
+// console.log(a+ +parseFloat(b));
+
+
+
+// let a=20
+// let b=40
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a+b);
+// console.log(a+b/a);
+
+
+
+
+
+// kvadratlar
+
+// let a=6
+// let b=5
+// console.log(a*b);
+// console.log(3*3*3);
+// console.log(a**b);
+
+
+
+
+// let a=3
+// let b="3"
+// console.log(a=b);
+// console.log(a==b);
+// console.log(a===3);
+
+
+
+
+
+// let a=3
+// let b=4
+// console.log(a=b);
+// console.log(a==b);
+// console.log(a===b);
+
+// let a=2
+// let c=3
+// c=a+c
+// c +=a
+// console.log(c);
+
+
+
+// let a=3
+// console.log(a++);
+// console.log(++a);
+// console.log(a);
+
+
+
+// let a=8
+//  console.log(a++);
+//  console.log(++a);   
+// console.log(a);
+
+
+
+// let a= 5
+// let b= 6
+// console.log(a>=b);
+// console.log(a<=b);
+// console.log(a !=b)
+// console.log(!(a!=b));
+// console.log(!false);
+
+
+
+
+let admin=true
+let teacher=true
+let student= false
+console.log(admin||student);
+console.log(admin||teacher);
