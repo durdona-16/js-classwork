d09b99e9fbaa623cf49eb45e360ceebac05cac5c
shohpolat <shohruxbeka@mail.ru>
2023-02-01 01:16:50 +0500

    if else

---
+++ 3-dars/index.js
@@ -0,0 +1,11 @@
+let number1 = 5;
+let number2 = 10;
+if (number1>number2) {
+    console.log(number1 + " katta " + number2 + " dan ");
+}
+else{
+    console.log(number2 + " katta " + number1 + " dan ");
+}
+// if elseda doim true holatini chiqaradi 
+// if true bolsa else false boladi 
+// tepadan pastga qarab uqiydi qaysi javob togri kelib qolsa uwa javobda toxtaladi
