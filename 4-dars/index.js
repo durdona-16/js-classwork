9c2a0353deb001664d08267cd201727f319b0e45
shohpolat <shohruxbeka@mail.ru>
2023-02-02 14:55:30 +0500

    switch for loop

---
+++ 4-dars/index.js
@@ -0,0 +1,70 @@
+// // let weather="yomgirli"
+// // weather=2
+// // weather>10
+// // switch(weather){
+// //     case"yomgir":
+// //     case"bulut":
+// //     console.log("bugun havo yomgirli");
+// //     break;
+// //     console.log("bugun havo bulut");
+
+// // }
+// let i=1
+// for(i;i<=20;i++){
+//     console.log(i);
+// }
+
+
+
+// let i=1
+// for(i;i<=20;i++){
+//     console.log(i);
+// }
+// // qiymati 1 dan 20 gacaha bolgan son chiqadi
+
+
+
+
+
+// let i=20
+// for(i;i>=1;i--){
+//     console.log(i);
+// }
+// qiymati 20dan pastga qarab tuwuvchi raqam hosil qilib beradi
+
+// let i=1
+// for(i;i<=20;i=i+2){
+//     console.log(i);
+// }
+// 1 dan 20 gacha bolgan toq sonlarni hosil qiladi
+
+
+
+
+//  for (let i=20;i>=1;i=i-2){
+//     console.log(i);
+//  }
+// qiymati 20 dan pastga qarab barcha  juft sonlarni chiqarib beradi
+
+
+
+
+
+//     for(let i=20;i>=1;i=i-3){
+//      console.log(i);
+// }
+// let i=1
+// for(i;i<=20;i++){
+//     console.log(i%2);
+
+// }0/1/\0/1
+// 
+// for(i;i<=20;i++){
+//     if(i%2===0){
+//          console.log(i,'Juft son')
+//         } 
+//      else 
+//      { console.log(i,"toq son")
+//     }
+  
+// }
