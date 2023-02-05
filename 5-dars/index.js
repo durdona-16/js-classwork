977866209bfcae470c37be745ae03293ac7437c3
shohpolat <shohruxbeka@mail.ru>
2023-02-05 23:46:34 +0500

    function excs

---
+++ 5-dars/index.js
@@ -0,0 +1,47 @@
+// function printJuft(num) {
+//     console.log(`${num} - Juft son`);
+
+// }
+
+// function printToq(num) {
+//     console.log(`${num} - Toq son`);
+
+// }
+
+
+// for(let i = 0; i < 50; i++){
+
+//     if(i%2 == 0){
+//         printJuft(i);
+//     }else{
+//         printToq(i);
+//     }
+
+// }
+let user = "shoxpolat"
+
+if(user == "shoxpolat"){
+
+    printFullname("Shohrux","Abdusamatov")
+    
+}
+
+if(user == "durdona"){
+
+    printFullname("Durdona", "Abdusamatova")
+
+}
+
+
+function printFullname(name,surname){
+    console.log(`ism: ${name}`);
+    console.log(`familya: ${surname}`);
+}
+
+
+
+
+
+
+
+
